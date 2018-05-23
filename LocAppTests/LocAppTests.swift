3086c9c71220a12e760a1860c291e45aa1cf00f7
Musa Lheureux <musalheureux@macbook-pro-de-musa.local>
2018-05-23 12:04:12 +0200

    Initial Commit

---
+++ LocAppTests/LocAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  LocAppTests.swift
+//  LocAppTests
+//
+//  Created by Musa Lheureux on 23/05/2018.
+//  Copyright © 2018 LocApp. All rights reserved.
+//
+
+import XCTest
+@testable import LocApp
+
+class LocAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
